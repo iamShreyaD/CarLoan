98777b7a98c9ecb8414064c1bf13a2d8b1452abd
Shreya B Deshpande <shreyavbdeshpande@gmail.com>
2023-03-19 23:53:15 -0400

    The description of the project has been added

--- CarLoan.java
+++ CarLoan.java
@@ -1,3 +1,12 @@
+/*
+ * Shreya B Deshpande
+ * This Project is done through Codecademy 'Learn: java'
+ * In this, project, a program has written to calculate the monthly car payment a user should expect
+ * to make after taking out a car loan.
+ * This CarLoan class includes car loan amount (carLoan), interest rate of the loan (interestRate),
+ * length of the loan (in years) (loanLength), and down payment (downPayment).
+ */
+
 
 public class CarLoan {
 	public static void main(String[] args) {
